55eb6c6e22c0edd0cfd83d5b5256377b46db8919
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 17:22:45 +0300

    create an implement State flow

---
+++ app/src/main/java/com/fighter/moviehub/utils/State.kt
@@ -0,0 +1,9 @@
+package com.fighter.moviehub.utils
+
+sealed class State<out T>{
+    data class Success<T>(val data : T) : State<T>()
+    data class Error(val message : String) : State<Nothing>()
+    object Loading : State<Nothing>()
+
+    fun toData() : T? = if (this is Success) data else null
+}
