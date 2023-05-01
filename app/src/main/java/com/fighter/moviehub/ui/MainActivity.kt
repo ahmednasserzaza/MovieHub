d731445f2ce2e3afaed9ba776e9c911e5916f5f9
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 19:11:32 +0300

    initiate viewModel in MainActivity

--- app/src/main/java/com/fighter/moviehub/ui/MainActivity.kt
+++ app/src/main/java/com/fighter/moviehub/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.fighter.moviehub.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.fighter.moviehub.R
+import com.fighter.moviehub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
