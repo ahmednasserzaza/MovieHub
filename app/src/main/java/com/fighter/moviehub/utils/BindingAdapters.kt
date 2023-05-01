9bdcfccb0a306dce6def566005aa3b6eed448554
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 19:00:12 +0300

    implement needed functions in BindingAdapters.kt

--- app/src/main/java/com/fighter/moviehub/utils/BindingAdapters.kt
+++ app/src/main/java/com/fighter/moviehub/utils/BindingAdapters.kt
@@ -1,2 +1,51 @@
 package com.fighter.moviehub.utils
 
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fighter.moviehub.R
+import com.fighter.moviehub.ui.base.BaseAdapter
+
+@BindingAdapter(value = ["app:showWhenLoading"])
+fun <T> showWhenLoading(view: View, state: State<T>?) {
+    if (state is State.Loading) {
+        view.visibility = View.VISIBLE
+    } else {
+        view.visibility = View.GONE
+    }
+}
+
+@BindingAdapter(value = ["app:showWhenSuccess"])
+fun <T> showWhenSuccess(view: View, state: State<T>?) {
+    if (state is State.Success) {
+        view.visibility = View.VISIBLE
+    } else {
+        view.visibility = View.GONE
+    }
+}
+
+@BindingAdapter(value = ["app:showWhenError"])
+fun <T> showWhenError(view: View, state: State<T>?) {
+    if (state is State.Error) {
+        view.visibility = View.VISIBLE
+    } else {
+        view.visibility = View.GONE
+    }
+}
+
+@BindingAdapter(value = ["app:imageUrl"])
+fun setImageFromUrl(view: ImageView, url: String?) {
+    Glide.with(view).load(Constant.IMAGE_PATH + url).error(R.drawable.ic_error)
+        .centerCrop().into(view)
+}
+
+@BindingAdapter(value = ["app:items"])
+fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
+    if (items != null) {
+        (view.adapter as BaseAdapter<T>?)?.setItems(items)
+    } else {
+        (view.adapter as BaseAdapter<T>?)?.setItems(emptyList())
+    }
+}
