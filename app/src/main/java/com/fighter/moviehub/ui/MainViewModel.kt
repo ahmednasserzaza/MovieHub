d96671a69251d9a85055664dded9c0c2729b0534
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 18:07:43 +0300

    prepare and implement MainViewModel

--- app/src/main/java/com/fighter/moviehub/ui/MainViewModel.kt
+++ app/src/main/java/com/fighter/moviehub/ui/MainViewModel.kt
@@ -1,4 +1,36 @@
 package com.fighter.moviehub.ui
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.fighter.moviehub.data.model.PopularMoviesResponse
+import com.fighter.moviehub.data.repositories.MovieRepository
+import com.fighter.moviehub.ui.base.BaseViewModel
+import com.fighter.moviehub.utils.State
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainViewModel : BaseViewModel() {
+    override val tag: String = this::class.java.simpleName
+    private val repository = MovieRepository()
+
+    private val _popularMovies = MutableLiveData<State<PopularMoviesResponse?>>()
+    val popularMovies: LiveData<State<PopularMoviesResponse?>> = _popularMovies
+
+    private fun getAllPopularMovies() {
+        val moviesDisposable = repository.getPopularMovies()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(::fetchPopularMovies, ::onError)
+
+        addToCompositeDisposable(moviesDisposable)
+    }
+
+    private fun fetchPopularMovies(state: State<PopularMoviesResponse?>) {
+        _popularMovies.postValue(state)
+    }
+
+    private fun onError(throwable: Throwable) {
+        log(throwable.message.toString())
+    }
+
 }
