dc9210fcbf303bc5ec3a7a397be8777afef85b32
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 17:02:18 +0300

    implement retrofit builder in API

--- app/src/main/java/com/fighter/moviehub/networking/API.kt
+++ app/src/main/java/com/fighter/moviehub/networking/API.kt
@@ -1,4 +1,16 @@
 package com.fighter.moviehub.networking
 
+import com.fighter.moviehub.utils.Constant
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 object API {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constant.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
+
+    val movieService: MovieService = retrofit.create(MovieService::class.java)
 }
