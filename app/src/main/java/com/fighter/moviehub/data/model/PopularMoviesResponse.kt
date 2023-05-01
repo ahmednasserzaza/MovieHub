235d25646acb23a620a6240fa52649a0799b4756
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 16:51:58 +0300

    prepare and implement popular movie model

--- app/src/main/java/com/fighter/moviehub/data/model/PopularMoviesResponse.kt
+++ app/src/main/java/com/fighter/moviehub/data/model/PopularMoviesResponse.kt
@@ -1,3 +1,10 @@
 package com.fighter.moviehub.data.model
 
-data class PopularMoviesResponse()
+import com.google.gson.annotations.SerializedName
+
+data class PopularMoviesResponse(
+    @SerializedName("page") val page: Int? = null,
+    @SerializedName("results") val movies: List<Movie?>? = null,
+    @SerializedName("total_pages") val totalPages: Int? = null,
+    @SerializedName("total_results") val totalResults: Int? = null
+)
