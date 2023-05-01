3ae561d61ed5fde10fcbbc10f528a7022b1725f8
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 17:41:45 +0300

    implement wrapper function in repository an add add function to get popular movies

--- app/src/main/java/com/fighter/moviehub/data/repositories/MovieRepository.kt
+++ app/src/main/java/com/fighter/moviehub/data/repositories/MovieRepository.kt
@@ -1,4 +1,26 @@
 package com.fighter.moviehub.data.repositories
 
+import com.fighter.moviehub.BuildConfig
+import com.fighter.moviehub.data.model.PopularMoviesResponse
+import com.fighter.moviehub.networking.API
+import com.fighter.moviehub.utils.State
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.Response
+
 class MovieRepository {
+    fun getPopularMovies(): Observable<State<PopularMoviesResponse?>> {
+        return wrapWithObservable { API.movieService.getPopularMovies(BuildConfig.API_KEY) }
+    }
+
+    private fun <T> wrapWithObservable(function: () -> Observable<Response<T>>): Observable<State<T?>> {
+        return function.invoke()
+            .map {
+                if (it.isSuccessful) {
+                    State.Success(it.body())
+                } else {
+                    State.Error(it.message())
+                }
+            }.onErrorReturn { State.Error(it.message.toString()) }
+            .startWithItem(State.Loading)
+    }
 }
