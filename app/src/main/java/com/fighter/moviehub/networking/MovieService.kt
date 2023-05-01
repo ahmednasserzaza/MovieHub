ea0df67ce1f7bb552c74b3ec14066ed377ef39df
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 16:56:54 +0300

    implement getPopularMovies function

--- app/src/main/java/com/fighter/moviehub/networking/MovieService.kt
+++ app/src/main/java/com/fighter/moviehub/networking/MovieService.kt
@@ -1,4 +1,14 @@
 package com.fighter.moviehub.networking
 
+import com.fighter.moviehub.data.model.PopularMoviesResponse
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieService {
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ): Observable<Response<PopularMoviesResponse>>
 }
