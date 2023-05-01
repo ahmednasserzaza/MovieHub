e1c6765279dce39f77bd10897c11758c163b2e6d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-01 17:44:29 +0300

    create and implement BaseViewModel

--- app/src/main/java/com/fighter/moviehub/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/fighter/moviehub/ui/base/BaseViewModel.kt
@@ -1,4 +1,24 @@
 package com.fighter.moviehub.ui.base
 
-class BaseViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+abstract class BaseViewModel : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    protected abstract val tag: String
+
+    protected fun addToCompositeDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    protected open fun log(message: String) {
+        Log.v(tag, message)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
